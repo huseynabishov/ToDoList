013b159c8ba7d7f9dd4de18cbca49130e4277bc7
huseynabishov <huseyn.abishov@icloud.com>
2022-04-16 20:04:13 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Huseyn Abishov on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
